330bf70a5ae2ca1aeac07a3d1ce1383e7773d410
a.n.darashevich <a.n.darashevich@gmail.com>
2022-06-26 12:21:50 +0300

    lesson 94 (React.Suspense and React.lazy)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React, {Component, ReactNode} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import classes from './components/Profile/ProfileInfo/Profile.module.css'
-import {BrowserRouter, Route, withRouter} from "react-router-dom";
+import {Route, withRouter} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import Login from "./components/login/Login";
 import {compose} from "redux";
-import {connect, Provider} from "react-redux";
+import {connect} from "react-redux";
 import {initializeApp} from "./redux/app-reducer";
-import store, {RootState} from "./redux/redux-store";
+import {RootState} from "./redux/redux-store";
 import Preloader from "./components/common/preloader/Preloader";
-import Footer from "./components/Footer/Footer";
-import {withSuspense} from "./hoc/WithSuspense";
-
-
-const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'))
-const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
-const Login = React.lazy(() => import('./components/login/Login'))
-const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'))
 
 export type MapDispatchToPropsType = {
     initializeApp: () => void
@@ -43,9 +39,8 @@ class App extends Component<MapDispatchToPropsType & MapStateToPropsType> {
 
     render() {
 
-        if (!this.props.initialized) {
-            return <Preloader/>
-        }
+        if(!this.props.initialized){
+            return <Preloader/>}
 
         return (
             <>
@@ -53,36 +48,19 @@ class App extends Component<MapDispatchToPropsType & MapStateToPropsType> {
                 <div className={classes.main}>
                     <Navbar/>
                     <div className={classes.MainContent}>
-                        <Route path="/Dialogs" render={withSuspense(DialogsContainer)}/>
-                        <Route path="/Profile/:userId?" render={withSuspense(ProfileContainer)}/>
-                        <Route path="/Users" render={withSuspense(UsersContainer)}/>
+                        <Route path="/Dialogs" render={() => <DialogsContainer/>}/>
+                        <Route path="/Profile/:userId?" render={() => <ProfileContainer/>}/>
+                        <Route path="/Users" render={() => <UsersContainer/>}/>
                         <Route path="/News" render={() => <News/>}/>
                         <Route path="/Music" render={() => <Music/>}/>
                         <Route path="/Settings" render={() => <Settings/>}/>
-                        <Route path="/Login" render={withSuspense(Login)}/>
+                        <Route path="/Login" render={() => <Login/>}/>
                     </div>
                 </div>
-                <Footer footerValue={"All rights reserved, 2022"}/>
             </>
         );
     }
 }
-
-compose<React.ComponentType>(
+export default compose<React.ComponentType>(
     withRouter,
     connect(MapStateToProps, {initializeApp}))(App)
-
-let AppContainer = compose<React.ComponentType>(
-    withRouter,
-    connect(MapStateToProps, {initializeApp}))(App)
-
-export const SamuraiJSApp = () => {
-    return (
-        <BrowserRouter>
-            <Provider store={store}>
-                <AppContainer/>
-            </Provider>
-        </BrowserRouter>
-    )
-}
-export default SamuraiJSApp
